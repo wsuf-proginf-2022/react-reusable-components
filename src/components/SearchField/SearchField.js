49e3df4f4e17019c40a2e40f883370dba130ffea
Zsolt Lengyel <zsolt.lengyel21@gmail.com>
2023-01-07 11:50:50 +0100

    lol digg

--- src/components/SearchField/SearchField.js
+++ src/components/SearchField/SearchField.js
@@ -2,7 +2,10 @@ import { useState, useRef, useEffect } from 'react';
 import './searchfield.css';
 
 const SearchFiled = ({ options = [] }) => {
+  // amire rá keresünk
   const [searchTerm, setSearchTerm] = useState('');
+  // az a ténylegesen létező elem amit kiválasztunk
+  const [selectedItem, setSelectedItem] = useState('');
   const [searchResults, setSearchResults] = useState(options);
   const [opened, setOpened] = useState(false);
   const container = useRef();
@@ -18,8 +21,14 @@ const SearchFiled = ({ options = [] }) => {
     };
   }, []);
 
+  function handleInput(input) {
+    setSearchTerm(input);
+    setOpened(false);
+    setSelectedItem(input);
+  }
+
   return (
-    <div className={`search-field ${opened ? '' : 'rounded'}`} ref={container}>
+    <div className={`search-field${opened ? '' : ' rounded'}`} ref={container}>
       <span className='search-icon' />
       <input
         className='input-real'
@@ -29,11 +38,19 @@ const SearchFiled = ({ options = [] }) => {
           setSearchTerm(event.target.value);
           setOpened(true);
         }}
+        onClick={() => {
+          setOpened(true);
+        }}
       />
       {opened && (
         <div className='options'>
           {searchResults.map((option) => (
-            <button className='option' onClick={() => {}} key={option}>
+            <button
+              className='option'
+              onClick={() => {
+                handleInput(option);
+              }}
+              key={option}>
               {option}
             </button>
           ))}
